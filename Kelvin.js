ada28c73565603a967ac81828354983602c5f42f
MarinaB93 <bogheanmarina93@gmail.com>
2021-06-24 09:05:15 +0300

    first commit

---
+++ Kelvin.js
@@ -0,0 +1,7 @@
+//This is a constant variable
+const kelvin = 293;
+var celsius = kelvin - 273; // celsius is 273 less than kelvin
+var fahrenheit = celsius * (9 / 5) + 32; // formula fahrenheit  which converts kelvin to f
+fahrenheit = Math.floor(fahrenheit); // this down the result
+let temperature = "TEMPERATURE";
+console.log("The temperature is" + ' ' + "TEMPERATURE" + ' ' + "degrees Fahrenheit.");
